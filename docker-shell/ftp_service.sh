5cdde04c8bc001aad664474dd129b36564f9e7a3
yiningzeng <yiningzeng@outlook.com>
2019-08-03 17:28:50 +0800

    feat: 创建ftp服务

---
+++ docker-shell/ftp_service.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo docker run -d -v /home/baymin/daily-work/ftp/:/home/vsftpd \
+                -p 20:20 -p 21:21 -p 47400-47470:47400-47470 \
+                -e FTP_USER=baymin \
+                -e FTP_PASS=baymin1024 \
+                -e PASV_ADDRESS=192.168.31.157 \
+                --name ftp \
+                --net ai \
+                --ip 10.10.0.2 \
+                --restart=always registry.cn-hangzhou.aliyuncs.com/baymin/remote-train:ftp
