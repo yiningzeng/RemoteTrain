428f481b95ee20095e4591afb4605d774adc7b45
yiningzeng <yiningzeng@outlook.com>
2019-08-07 16:53:30 +0800

    fix: rabbitmq shell脚本

--- docker-shell/rabbitmq_service.sh
+++ docker-shell/rabbitmq_service.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-sudo docker run -d --name rabbitmq \
-            -p 5672:5672 -p 15672:15672 \
-            -e RABBITMQ_DEFAULT_USER=baymin \ # 可设可不设，默认是baymin
-            -e RABBITMQ_DEFAULT_PASS=baymin1024 \  # 可设可不设，默认是baymin1024
-            --net ai \
-            --ip 10.10.0.3 \
-            --restart=always \
-            registry.cn-hangzhou.aliyuncs.com/baymin/ai-power:rabbitmq
+# RABBITMQ_DEFAULT_USER可设可不设，默认是baymin
+# RABBITMQ_DEFAULT_PASS可设可不设，默认是baymin1024
+ sudo docker run -d -p 5672:5672 -p 15672:15672 --name rabbitmq -e RABBITMQ_DEFAULT_USER=baymin -e RABBITMQ_DEFAULT_PASS=baymin1024 --net ai --ip 10.10.0.3 --restart=always registry.cn-hangzhou.aliyuncs.com/baymin/ai-power:rabbitmq
