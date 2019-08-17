8c7f238c87f542bb678db79d0cc6465256a7ad5c
yiningzeng <yiningzeng@outlook.com>
2019-08-17 18:32:23 +0800

    feat: 新增gpus

--- docker-shell/darknet-dockerfile/start.sh
+++ docker-shell/darknet-dockerfile/start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cd /darknet
+use_gpus=`cat assets/use_gpus`
 python voc2DarknetAssets-python2.py
 if [ -f "assets/backup/yolov3-voc_last.weights" ];then
 echo "yolov3-voc_last.weights exist"
-./darknet detector train assets/voc.data assets/yolov3-voc.cfg assets/backup/yolov3-voc_last.weights -dont_show
+./darknet detector train assets/voc.data assets/yolov3-voc.cfg assets/backup/yolov3-voc_last.weights -dont_show -gpus $use_gpus
 else
 echo "yolov3-voc_last.weights not exist, use darknet53.conv.74"
-./darknet detector train assets/voc.data assets/yolov3-voc.cfg darknet53.conv.74 -dont_show
+./darknet detector train assets/voc.data assets/yolov3-voc.cfg darknet53.conv.74 -dont_show -gpus $use_gpus
 fi
 ok=$?
 if [ $ok == 0 ];then
--- docker-shell/darknet-dockerfile/test.sh
+++ docker-shell/darknet-dockerfile/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-/home/baymin/daily-work/darknet-license/darknet detector train /home/baymin/daily-work/new-work/ab-darknet/yunsheng7-24/voc.data /home/baymin/daily-work/new-work/ab-darknet/yunsheng7-24/yolov3-voc.cfg /home/baymin/daily-work/new-work/ab-darknet/yunsheng7-24/darknet53.conv.74 -dont_show
+use_gpus=`cat use_gpus`
+/home/baymin/daily-work/darknet-license/darknet detector train /home/baymin/daily-work/new-work/ab-darknet/yunsheng7-24/voc.data /home/baymin/daily-work/new-work/ab-darknet/yunsheng7-24/yolov3-voc.cfg /home/baymin/daily-work/new-work/ab-darknet/yunsheng7-24/darknet53.conv.74 -dont_show -gpus $use_gpus
 ok=$?
 if [ $ok == 0 ];then
 	echo "正常退出"
