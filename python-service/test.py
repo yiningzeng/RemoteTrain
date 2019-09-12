53c108a4444394740969f3e9cf23dd183ed7f65d
yiningzeng <yiningzeng@outlook.com>
2019-09-12 16:39:26 +0800

    feat: test

--- python-service/test.py
+++ python-service/test.py
@@ -1,10 +1,27 @@
-import requests
+from urllib import parse
+from urllib import request
+import glob
 import os
+from PIL import Image
 
 if __name__ == '__main__':
-    projectId = os.popen('cat ../aa/project_id22.log').read().replace('\n', '')
-    if projectId == "":
-        print("aasdsd")
-    json_data = [{"x": 13.00, "y": 13.33, "win_id": "asdasdsadsadsaddsa-123", "title": "asdasdsadsadsaddsa-123"}]
-    r11 = requests.post("http://192.168.31.75:18888/draw_chart", json=json_data)
-    print(r11.json())
+    file_list = glob.glob('/home/baymin/daily-work/new-work/素材/yunsheng_date/2019-9-9/工位5-1/*.json')
+    for num, fileName in enumerate(file_list):
+        if os.path.exists(fileName):  # 如果文件存在
+            f = open(fileName, 'rb')  # 里面为文件路径
+            newcontent = str(f.read(), encoding = "utf-8").replace("C:/Users/admin/Desktop/%E5%B7%A5%E4%BD%8D5-1", "${path}")
+            f.close()
+
+            # 打开「detail_content」文件
+            fout = open(fileName, 'w')
+            # 写入文件内容
+            fout.write(str(newcontent))
+            fout.close()
+
+            # newFileName = parse.unquote(fileName)  # 解码url
+            # print(newFileName)
+            # im = Image.open(fileName)
+            # im.save(newFileName)
+            # os.remove(fileName)
+        else:
+            print('no such file:%s' % fileName)  # 则返回文件不存在
