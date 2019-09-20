952a4f4609f51eec224356abef3ea83d0a531b07
yiningzeng <yiningzeng@outlook.com>
2019-09-20 10:53:08 +0800

    feat: 自增外网的测试

---
+++ docker-shell/web_test_outside_service.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo  docker run \
+            --name service-web-test-outside \
+            -p 81:80 \
+            -v /opt/remote_train_web/test:/usr/local/apache2/htdocs/ \
+            --net ai --ip 10.10.0.7 \
+            --add-host service-postgresql:10.10.0.4 \
+            --add-host service-rabbitmq:10.10.0.3 \
+            --add-host service-ftp:10.10.0.2 \
+            --add-host service-web:10.10.0.5 \
+            --restart=always \
+            -d registry.cn-hangzhou.aliyuncs.com/baymin/remote-train:web-v3.5
